cf78185df6bcbf3f4611db018d8f23b8aff5d399
esid <sidorenko.evg@gmail.com>
2015-10-10 23:52:23 +0200

    initialize project with gitignore

---
+++ src/main/java/com/sid/Svet.java
@@ -0,0 +1,13 @@
+package com.sid;
+
+/**
+ * Hello world!
+ *
+ */
+public class Svet 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
